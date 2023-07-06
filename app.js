786ef49b386d580bee0b7709652a10133e1e61c5
perd007 <95108549+perd007@users.noreply.github.com>
2023-07-06 20:21:32 +0000

    corrigiendo algunos comentarios

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const fromEuroToDollar = function(valueInEuro){
     let valueInDollar = valueInEuro * 1.2;
     // retornamos el valor
     return valueInDollar;
-}
+}ç
+// declaramos una funcion con el mismo nombre "fromDollarToYen"
 const fromDollarToYen = function(valueInEuro){
     // convertimos el valor a dolares
     let valueInYen = valueInEuro * 127.9;
     // retornamos el valor
     return Number(valueInYen.toFixed(2));
 }
+// declaramos una funcion con el mismo nombre "fromYenToPound "
 const fromYenToPound = function(valueInEuro){
     // convertimos el valor a dolares
     let valueInPound = valueInEuro * 0.8;
@@ -23,7 +25,7 @@ const sum= (a,b)=> {
     return a + b;
 }
 
-console.log(sum(7,3));
-console.log(fromYenToPound(3.5));
+//console.log(sum(7,3));
+
 
 module.exports ={sum, fromEuroToDollar, fromDollarToYen,fromYenToPound };
